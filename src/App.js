5721d89e5fbc95e4860ae155674765d15a751ad6
Kim Young Min <kastori1990@gmail.com>
2023-03-14 13:24:32 +0900

    메소드 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-// App.js
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { getTodos, getTodo, postTodo, updateTodo } from "./request";
 
 function App() {
   const [data, setData] = useState([]);
 
-  const handler = async () => {
-    const res = await fetch("/api", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(res);
-    setData(res);
+  const handleGetTodos = async () => {
+    const todos = await getTodos();
+    setData(todos);
   };
+
+  const handleGetTodo = async (id) => {
+    const todo = await getTodo(id);
+    setData(todo);
+  };
+
+  const handlePostTodo = async () => {
+    const newTodo = await postTodo();
+    setData(newTodo);
+  };
+
+  const handleUpdateTodo = async (id) => {
+    const updatedTodo = await updateTodo(id);
+    setData(updatedTodo);
+  };
+
   useEffect(() => {
     console.log(data);
   }, [data]);
 
   return (
     <div>
-      <button onClick={handler}>notion get</button>
+      <button onClick={handleGetTodos}>get Todos</button>
+      <button onClick={() => handleGetTodo(1)}>get Todo one</button>
+      <button onClick={handlePostTodo}>post Todo</button>
+      <button onClick={() => handleUpdateTodo(1)}>update Todo</button>
+      <button onClick={() => console.log(data)}>현재 data 출력</button>
     </div>
   );
 }
