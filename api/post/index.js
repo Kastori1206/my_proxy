d007ee4fddcc90135fd6cd637cd30075cdefacf5
Kim Young Min <kastori1990@gmail.com>
2023-03-14 13:27:49 +0900

    notion api 테스트

--- api/post/index.js
+++ api/post/index.js
@@ -28,21 +28,35 @@
 //   return response.status(200).json(data);
 // }
 import fetch from "node-fetch";
+import { TOKEN, DATABASE_ID } from "../../config";
 
 // url/api/post 처럼 사용
 export default async function postTodo(request, response) {
   if (request.method !== "POST") {
     return response.status(400).json("method Error");
   }
-
-  const { body } = request;
-  const res = await fetch(process.env.BASE_URL, {
+  const options = {
     method: "POST",
     headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
       "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
     },
-    body: JSON.stringify(body),
-  });
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "WorkPeriod",
+          direction: "descending",
+        },
+      ],
+    }),
+  };
+  const { body } = request;
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
   const data = await res.json();
   return response.status(200).json(data);
 }
