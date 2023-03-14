3ab886126eaf8ff47e656e154bced6caa9b836ab
Kim Young Min <kastori1990@gmail.com>
2023-03-14 11:41:21 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [loaded, setLoaded] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.data);
+        setLoaded(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return loaded ? (
+    <ol>
+      {data.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ol>
+  ) : (
+    "데이터 fetch 미완료"
   );
 }
 
